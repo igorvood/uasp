58e82367540debad0cd717430b0deb7012161d43
vood <balitskY_igor@list.ru>
2023-02-06 16:17:40 +0300

    4

--- uasp-streaming-common/src/main/scala/ru/vtb/uasp/common/mask/JsStringMaskedFun.scala
+++
@@ -1,9 +0,0 @@
-package ru.vtb.uasp.common.mask
-
-import play.api.libs.json.{JsBoolean, JsNumber, JsString}
-
-trait JsStringMaskedFun extends MaskedFun[JsString]
-
-trait JsNumberMaskedFun extends MaskedFun[JsNumber]
-
-trait  JsBooleanMaskedFun extends MaskedFun[JsBoolean]
--- uasp-streaming-common/src/main/scala/ru/vtb/uasp/common/mask/MaskedFun.scala
+++ uasp-streaming-common/src/main/scala/ru/vtb/uasp/common/mask/MaskedFun.scala
@@ -1,9 +1,12 @@
 package ru.vtb.uasp.common.mask
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsBoolean, JsNumber, JsString, JsValue}
 
-trait MaskedFun[T<: JsValue] extends ((T) => T) {
+trait MaskedFun[T<: JsValue] extends (T => T)
 
+trait JsStringMaskedFun extends MaskedFun[JsString]
 
+trait JsNumberMaskedFun extends MaskedFun[JsNumber]
 
-}
+trait  JsBooleanMaskedFun extends MaskedFun[JsBoolean]
+
