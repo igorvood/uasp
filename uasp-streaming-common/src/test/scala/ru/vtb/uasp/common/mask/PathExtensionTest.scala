870e6e05a727d21f7245d80722b8fd953ffe712a
vood <balitskY_igor@list.ru>
2023-02-06 10:57:13 +0300

    4

--- uasp-streaming-common/src/test/scala/ru/vtb/uasp/common/mask/PathExtensionTest.scala
+++ uasp-streaming-common/src/test/scala/ru/vtb/uasp/common/mask/PathExtensionTest.scala
@@ -35,10 +35,39 @@ class PathExtensionTest extends AnyFlatSpec with should.Matchers {
     )
       .map(MaskedStrPath)
       .toJsonPath()
-
     assertResult(root)(path)
+  }
 
+  "transform str to JPath " should " no ERR when dublicate" in {
+    val root = JPathObject("root",
+      Set(
+        JPathObject("f1",
+          Set(JPathObject("o1", Set(
+            JPathValue("d1"),
+            JPathValue("d2"),
+            JPathValue("d3")
+          )))),
+        JPathObject("f2",
+          Set(
+            JPathObject("o2", Set(
+              JPathValue("d3")
+            )
+            )
+          )
+        )
+      )
+    )
 
+    val path = List(
+      "f1.o1.d1",
+      "f1.o1.d2",
+      "f1.o1.d3",
+      "f2.o2.d3",
+      "f2.o2.d3",
+    )
+      .map(MaskedStrPath)
+      .toJsonPath()
+    assertResult(root)(path)
   }
 
 }
