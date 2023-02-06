f2c267d525d2a05991bfbd3c862f115d626de1ac
vood <balitskY_igor@list.ru>
2023-02-06 11:14:43 +0300

    4

---
+++ uasp-streaming-common/src/test/scala/ru/vtb/uasp/common/mask/MaskedTest.scala
@@ -0,0 +1,49 @@
+package ru.vtb.uasp.common.mask
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import play.api.libs.json.{JsObject, JsValue, Json}
+import ru.vtb.uasp.common.dto.UaspDto
+import ru.vtb.uasp.common.mask.JPath.PathFactory
+
+
+class MaskedTest extends AnyFlatSpec with should.Matchers {
+
+  def maskData(jsObject: JsValue, path: JPath): JsValue = {
+
+
+    ???
+  }
+
+  "transform str to JPath " should " OK" in {
+
+    val dto = UaspDto(
+      id = "1",
+      dataInt = Map("2" -> 2),
+      dataLong = Map("3" -> 3),
+      dataFloat = Map("4" -> 4),
+      dataDouble = Map("5" -> 5),
+      dataDecimal = Map("6" -> 6),
+      dataString = Map("7" -> "7"),
+      dataBoolean = Map("8" -> false),
+      uuid = "uuid",
+      process_timestamp = 18
+    )
+
+    val jsObject: JsValue = Json.toJsObject(dto)
+    println(jsObject)
+
+    val path = List(
+      "id",
+      "dataString.7",
+    ).map(MaskedStrPath)
+      .toJsonPath()
+
+    maskData(jsObject, path)
+
+
+  }
+
+
+
+}
