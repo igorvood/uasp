64fd72e5a1e19f1b03e4fba1df670d9aef256a74
vood <balitskY_igor@list.ru>
2023-02-13 11:36:16 +0300

    проба подключения маскирования к продьюссеру

--- uasp-streaming-common/src/main/scala/ru/vtb/uasp/common/mask/dto/JsMaskedPathError.scala
+++ uasp-streaming-common/src/main/scala/ru/vtb/uasp/common/mask/dto/JsMaskedPathError.scala
@@ -1,3 +1,11 @@
 package ru.vtb.uasp.common.mask.dto
 
+import play.api.libs.json.{Json, OWrites, Reads}
+
 case class JsMaskedPathError(error: String)
+
+object JsMaskedPathError{
+  implicit val uaspJsonReads: Reads[JsMaskedPathError] = Json.reads[JsMaskedPathError]
+  implicit val uaspJsonWrites: OWrites[JsMaskedPathError] = Json.writes[JsMaskedPathError]
+
+}
