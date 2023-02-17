af2724efc51d779af18ce370b2222726815f1850
vood <balitskY_igor@list.ru>
2023-02-17 10:31:46 +0300

    q

--- uasp-streaming-input-convertor/src/main/scala/ru/vtb/uasp/inputconvertor/entity/CommonMessageType.scala
+++ uasp-streaming-input-convertor/src/main/scala/ru/vtb/uasp/inputconvertor/entity/CommonMessageType.scala
@@ -5,13 +5,13 @@ import org.json4s._
 import java.time.{LocalDateTime, ZoneId}
 
 case class CommonMessageType(
-                              message_key: String,
+                              message_key: String, // используется
                               message: Array[Byte],
                               message_str: Option[String] = None,
                               json_message: Option[JValue] = None,
                               json_schemakey: Option[String] = None,
                               json_schema: Option[String] = None,
-                              avro_message: Option[Array[Byte]] = None,
+                              avro_message: Option[Array[Byte]] = None, // используется
                               error: Option[String] = None,
                               valid: Boolean = false,
 
